2e38eb2b95043d2795a8e6dc63218ca003378e45
Harpal <Harpalsgh@gmail.com>
2019-11-27 15:27:42 -0600

    eureka,zuul

--- src/main/java/com/cde/microservices/currencycalculationservice/CurrencyCalculationController.java
+++ src/main/java/com/cde/microservices/currencycalculationservice/CurrencyCalculationController.java
@@ -1,19 +1,55 @@
 package com.cde.microservices.currencycalculationservice;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.cde.microservices.currencycalculationservice.bean.CurrencyConversionBean;
 
 @RestController
 public class CurrencyCalculationController {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private CurrencyExchangeServiceProxy proxy;
 
 	@GetMapping("/currency-convertor/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 			
-		return new CurrencyConversionBean(1L, from, to, BigDecimal.ONE, quantity,quantity, 0);
+		Map<String, String> uriVariables = new HashMap<String, String>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
+				CurrencyConversionBean.class, 
+				uriVariables);
+		
+		CurrencyConversionBean response = responseEntity.getBody();
+		
+		return new CurrencyConversionBean(response.getId(), from, to , 
+				response.getConversionRate(), quantity,
+				quantity.multiply(response.getConversionRate()), response.getPort());
+	}
+	
+	@GetMapping("/currency-convertor-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {	
+		
+		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+		
+		logger.info("{}", response);
+		
+		return new CurrencyConversionBean(response.getId(), from, to , 
+				response.getConversionRate(), quantity,
+				quantity.multiply(response.getConversionRate()), response.getPort());
 	}
 }
